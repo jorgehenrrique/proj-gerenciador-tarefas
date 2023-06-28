3e1874d16d3ea6dc44f4fc2f9bd8bdba206d0d80
Jorge H <jorgehenrrique@live.com>
2023-06-27 22:19:55 -0300

    criando add tarefas

--- src/assets/pages/Cadastrar/cadastrarTarefas.jsx
+++ src/assets/pages/Cadastrar/cadastrarTarefas.jsx
@@ -11,7 +11,23 @@ export default function CadastrarTarefas() {
     <div className="container-cadastrar">
       <Breadcrumb />
 
-      <h3>Cadastrar</h3>
+      <section className='cadastrar'>
+        <form>
+          <label htmlFor="nome">Nome</label>
+          <input type="text" id='nome' />
+
+          <label htmlFor="select">Select</label>
+          <select id='select'>
+            <option value="" selected disabled></option>
+            <option value="LISTADA">LISTADA</option>
+            <option value="INICIADA">INICIADA</option>
+            <option value="FINALIZADA">FINALIZADA</option>
+          </select>
+
+          <label htmlFor="descricao">Descrição</label>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </form>
+      </section>
     </div>
   );
 }
--- src/assets/pages/Editar/editarTarefas.jsx
+++ src/assets/pages/Editar/editarTarefas.jsx
@@ -1,3 +1,4 @@
+import Breadcrumb from '../../components/Breadcrumb/breadcrumb';
 import './editarTarefas.css';
 import { useLocation } from 'react-router-dom';
 
@@ -6,8 +7,14 @@ export default function EditarTarefas() {
   const { pathname } = useLocation();
   console.log(pathname)
 
+  const { state } = useLocation();
+  console.log(state)
+
   return (
     <div className="container-editar">
+      <Breadcrumb />
+
+
       <h2>editar</h2>
     </div>
   );
