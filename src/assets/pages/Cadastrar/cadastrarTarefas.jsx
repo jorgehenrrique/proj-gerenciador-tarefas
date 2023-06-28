18e1a39831594b656588891b043de476fa767502
Jorge H <jorgehenrrique@live.com>
2023-06-27 23:00:31 -0300

    criando formulario

--- src/assets/pages/Cadastrar/cadastrarTarefas.jsx
+++ src/assets/pages/Cadastrar/cadastrarTarefas.jsx
@@ -1,34 +1,31 @@
 import { useLocation } from 'react-router-dom';
 import './cadastrarTarefas.css';
 import Breadcrumb from '../../components/Breadcrumb/breadcrumb';
+import { useRef } from 'react';
 
 export default function CadastrarTarefas() {
 
   const { state } = useLocation();
   console.log(state)
 
+  const nome = useRef(null);
+  const opcao = useRef(null);
+  const descricao = useRef(null);
+
   function handlerSubmit(e) {
     e.preventDefault();
-    // if (!name.current.value.trim()) {
-    //   alert("Informe um nome correto!");
-    //   return;
-    // }
-    // if (!email.current.value.trim()) {
-    //   alert("Informe um Email correto!");
-    //   return;
-    // }
-    // if (!password.current.value.trim() || !passwordConfirm.current.value.trim()) {
-    //   alert("Informe as senhas nos dois campos!");
-    //   return;
-    // }
-    // if (password.current.value.trim() !== passwordConfirm.current.value.trim()) {
-    //   alert("Campos de senha inválidos!");
-    //   return;
-    // }
-    // if (!checkboxContirm.current.checked) {
-    //   alert("Aceite os termos de uso!");
-    //   return;
-    // }
+    if (!nome.current.value.trim()) {
+      alert("Informe um nome!");
+      return;
+    }
+    if (!opcao.current.value) {
+      alert("Selecione uma opção!");
+      return;
+    }
+    if (!descricao.current.value.trim()) {
+      alert("Adicione uma descrição!");
+      return;
+    }
   }
 
   return (
@@ -38,10 +35,10 @@ export default function CadastrarTarefas() {
       <section className='cadastrar'>
         <form className='form' onSubmit={handlerSubmit}>
           <label htmlFor="nome">Nome</label>
-          <input type="text" id='nome' />
+          <input type="text" id='nome' ref={nome} />
 
           <label htmlFor="select">Select</label>
-          <select id='select'>
+          <select id='select' ref={opcao}>
             <option value="" selected disabled hidden></option>
             <option value="LISTADA">LISTADA</option>
             <option value="INICIADA">INICIADA</option>
@@ -49,7 +46,7 @@ export default function CadastrarTarefas() {
           </select>
 
           <label htmlFor="descricao">Descrição</label>
-          <textarea id="descricao" cols="30" rows="10"></textarea>
+          <textarea ref={descricao} id="descricao" cols="30" rows="10"></textarea>
 
           <div>
             <button>Cadastrar</button>
