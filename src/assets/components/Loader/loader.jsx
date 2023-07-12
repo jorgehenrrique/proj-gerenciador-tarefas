8dfb08e75e21ab00252a399f4ea087c154cbbc39
Jorge H <jorgehenrrique@live.com>
2023-07-12 00:11:20 -0300

    Testando novo loader

--- src/assets/components/Loader/loader.jsx
+++ src/assets/components/Loader/loader.jsx
@@ -3,8 +3,9 @@ import './loader.css';
 export default function Loader() {
 
   return (
-    <div className="spinner">
-      <div className="spinner1"></div>
-    </div>
+    // <div className="spinner">
+    //   <div className="spinner1"></div>
+    // </div>
+    <div className="loader"></div>
   );
 }
