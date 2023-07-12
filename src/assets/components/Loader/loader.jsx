171ab695b6bcf5c89bc4a9336521f470ffc46876
Jorge H <jorgehenrrique@live.com>
2023-07-12 14:39:30 -0300

    rmv loader antigo

--- src/assets/components/Loader/loader.jsx
+++ src/assets/components/Loader/loader.jsx
@@ -3,9 +3,6 @@ import './loader.css';
 export default function Loader() {
 
   return (
-    // <div className="spinner">
-    //   <div className="spinner1"></div>
-    // </div>
     <div className="loader"></div>
   );
 }
