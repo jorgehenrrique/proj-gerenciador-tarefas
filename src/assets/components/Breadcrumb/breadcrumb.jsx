025cb6a5a8fb46b80bdb7beef38fcfb4ba070680
Jorge H <jorgehenrrique@live.com>
2023-07-12 16:40:05 -0300

    add comentario breadcrumb

--- src/assets/components/Breadcrumb/breadcrumb.jsx
+++ src/assets/components/Breadcrumb/breadcrumb.jsx
@@ -7,7 +7,8 @@ export default function Breadcrumb() {
   const path = pathname.split('/').filter(part => part !== ''); // Remover partes vazias
 
   const formattedPath = path.map(part => {
-    return part.charAt(0).toUpperCase() + part.slice(1);
+    return part.charAt(0).toUpperCase() + part.slice(1); // Deixar primeiro caractere maiusculo
+    // indice 0 fica maiusculo, + o restante do indice 1 para frente
   });
 
   return (
